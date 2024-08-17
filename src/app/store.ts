9ce4ddf006d582a45cc9f05f839cc5f3d5d1c6dc
Aleksandr Ufimtsev <fimak@bk.ru>
2024-08-17 17:03:27 +0700

    notes state and action

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface AppState {
   selectedOption: string;
   setSelectedOption: (option: string) => void;
+  notes: string;
+  setNotes: (notes: string) => void;
 }
 
 export const useAppStore = create<AppState>((set) => ({
   selectedOption: "",
   setSelectedOption: (option) => set({ selectedOption: option }),
+  notes: "",
+  setNotes: (notes) => set({ notes }),
 }));
